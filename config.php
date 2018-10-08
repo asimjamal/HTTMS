e56799a5a00034cae125df7147cc435a9263fd58
Asim Jamal <jamalasim081@gmail.com>
2018-10-08 16:51:01 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+  $config = array (
+  'db_host' => '127.0.0.1',
+  'db_name' => 'httms3',
+  'db_user' => 'master',
+  'db_pswd' => '12345678',
+  'ldap_host' => '',
+  'ldap_dn' => '',
+);
+?>
